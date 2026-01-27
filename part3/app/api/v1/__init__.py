e4aed064ad3dbfa7ed95d3629017c59a89cc6efe
Ali Abdullah <aliabdullah1215@icloud.com>
2026-01-27 11:51:25 +0000

    update

--- part3/app/api/v1/__init__.py
+++ part3/app/api/v1/__init__.py
@@ -4,14 +4,14 @@ from app.api.v1.users import api as users_api
 from app.api.v1.auth import api as auth_api
 from app.api.v1.protected import api as protected_api
 from app.api.v1.places import api as places_api
-api.add_namespace(places_api)
 
 api = Api(
     title='HBnB API',
     version='1.0',
-    description='HBnB API'
+    description='HBnB REST API'
 )
 
 api.add_namespace(users_api, path='/api/v1/users')
 api.add_namespace(auth_api, path='/api/v1/auth')
 api.add_namespace(protected_api, path='/api/v1/protected')
+api.add_namespace(places_api, path='/api/v1/places')
--- part3/app/services/facade.py
+++ part3/app/services/facade.py
@@ -1,11 +1,13 @@
 from app.persistence.repository import InMemoryRepository
 from app.models.user import User
+from app.models.place import Place
 
 
 class HBnBFacade:
     def __init__(self):
         # Repositories
         self.user_repo = InMemoryRepository()
+        self.place_repo = InMemoryRepository()
 
     # -------- User methods --------
     def create_user(self, user_data):
@@ -24,4 +26,34 @@ class HBnBFacade:
 
     def get_user_by_email(self, email):
         return self.user_repo.get_by_attribute('email', email)
+
+    # -------- Place methods --------
+    def create_place(self, data):
+        """
+        data must contain:
+        title, description, price, latitude, longitude, owner_id
+        """
+        owner = self.get_user(data['owner_id'])
+        if not owner:
+            raise ValueError("Owner not found")
+
+        place = Place(
+            title=data['title'],
+            owner=owner,
+            description=data.get('description', ''),
+            price=data.get('price', 0),
+            latitude=data.get('latitude'),
+            longitude=data.get('longitude')
+        )
+        self.place_repo.add(place)
+        return place
+
+    def get_place(self, place_id):
+        return self.place_repo.get(place_id)
+
+    def get_all_places(self):
+        return self.place_repo.get_all()
+
+    def update_place(self, place_id, data):
+        return self.place_repo.update(place_id, data)
 
