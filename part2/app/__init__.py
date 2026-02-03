b31f2c8b415b0b70854e94b564f8a2189163a78e
Ali Summan <144675922+aliabdullah1215@users.noreply.github.com>
2026-02-03 19:10:04 +0300

    Update API namespace paths for resources

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -18,10 +18,10 @@ def create_app():
     )
 
     # Register namespaces
-    api.add_namespace(users_ns, path='/api/v1/users')
-    api.add_namespace(amenities_ns, path='/api/v1/amenities')
-    api.add_namespace(places_ns, path='/api/v1/places')
-    api.add_namespace(reviews_ns, path='/api/v1/reviews')
+    api.add_namespace(users_ns, path='/users')
+    api.add_namespace(amenities_ns, path='/amenities')
+    api.add_namespace(places_ns, path='/places')
+    api.add_namespace(reviews_ns, path='/reviews')
     api.add_namespace(status_ns, path='/status')
     return app
 
