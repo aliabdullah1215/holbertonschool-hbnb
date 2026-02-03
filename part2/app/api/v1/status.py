1c04b6085694d33e596dd8e80a01e7530d51af02
Ali Summan <144675922+aliabdullah1215@users.noreply.github.com>
2026-02-03 19:27:28 +0300

    Delete part2/app/api/v1/status.py

--- part2/app/api/v1/status.py
+++
@@ -1,8 +0,0 @@
-from flask_restx import Namespace, Resource
-
-api = Namespace("status", description="API Status")
-
-@api.route("/")
-class StatusResource(Resource):
-    def get(self):
-        return {"status": "OK"}, 200
