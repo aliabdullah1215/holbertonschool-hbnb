1f974df63a9041ede3b25a2dbaca98366ea2a269
Ali Summan <144675922+aliabdullah1215@users.noreply.github.com>
2026-02-04 03:23:12 +0300

    Update run.py

--- part3/run.py
+++ part3/run.py
@@ -1,10 +1,6 @@
 from app import create_app
-from app.api.v1 import api as api_v1
 
 app = create_app()
 
-# Register API v1
-api_v1.init_app(app)
-
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
