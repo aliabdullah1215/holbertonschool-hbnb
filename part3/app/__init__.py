2323f232ae2b5b26b7760403991b999f72d04c2d
Ali Summan <144675922+aliabdullah1215@users.noreply.github.com>
2026-02-04 18:35:05 +0300

    Update __init__.py

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -33,11 +33,13 @@ def create_app(config_class="config.DevelopmentConfig"):
         from app.api.v1.auth import api as auth_ns
         from app.api.v1.amenities import api as amenities_ns
         from app.api.v1.places import api as places_ns
-
+        from app.api.v1.reviews import api as reviews_api
+        
         # 4. تسجيل الـ Namespaces في المسارات المحددة
         api.add_namespace(users_ns, path='/api/v1/users')
         api.add_namespace(auth_ns, path='/api/v1/auth')
         api.add_namespace(amenities_ns, path='/api/v1/amenities')
         api.add_namespace(places_ns, path='/api/v1/places')
-
+        api.add_namespace(reviews_api, path='/reviews')
+        
     return app
