25be9802c4d3d81b12e397a04d24f9b8bd2febcb
Ali Summan <144675922+aliabdullah1215@users.noreply.github.com>
2026-02-04 03:20:24 +0300

    Update __init__.py

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
+from flask_restx import Api # إضافة هذه المكتبة
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
@@ -15,5 +16,18 @@ def create_app(config_class="config.DevelopmentConfig"):
     db.init_app(app)
     bcrypt.init_app(app)
     jwt.init_app(app)
+
+    # تعريف الـ Api الرئيسي
+    api = Api(app, version='1.0', title='HBnB API', 
+              description='HBnB Application API', doc='/api/v1/')
+
+    # استيراد وتسجيل الـ Namespaces
+    from app.api.v1.users import api as users_ns
+    from app.api.v1.auth import api as auth_ns
+    from app.api.v1.amenities import api as amenities_ns
+
+    api.add_namespace(users_ns, path='/api/v1/users')
+    api.add_namespace(auth_ns, path='/api/v1/auth')
+    api.add_namespace(amenities_ns, path='/api/v1/amenities')
 
     return app
